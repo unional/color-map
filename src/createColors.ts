1a12eb8e42867bf9efeb7251ec240e967c1b0cf7
Homa Wong <homawong@gmail.com>
2017-09-30 02:10:33 -0700

    chore: cleanup code to bring coverage back up

--- src/createColors.ts
+++ src/createColors.ts
@@ -8,20 +8,19 @@ export function createColors(from: RGB, to: RGB, shades: number, alpha: Alpha = 
     to[1] - from[1],
     to[2] - from[2]
   ]
-  if (alpha) {
-    start.push(alpha[0])
-    diff.push(alpha[1] - alpha[0])
-  }
+
+  start.push(alpha[0])
+  diff.push(alpha[1] - alpha[0])
 
   const inc = 1 /
-  Math.max(shades - 1, 1)
+    Math.max(shades - 1, 1)
 
   for (let i = 0; i < shades; i++) {
     const color = [
       Math.round(start[0] + i * diff[0] * inc),
       Math.round(start[1] + i * diff[1] * inc),
       Math.round(start[2] + i * diff[2] * inc),
-      alpha ? start[3] + i * diff[3] * inc : 1
+      start[3] + i * diff[3] * inc
     ]
     rgba.push(color)
   }

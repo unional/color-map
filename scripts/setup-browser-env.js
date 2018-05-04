9457aedf7e8f6e65681e81f51e931c20e5c5955a
Homa Wong <homawong@gmail.com>
2018-05-04 00:19:54 -0700

    fix: add src back to package. (#53)
    webpack + ts-loader + source-map-loader can use it to generate correct source map

--- scripts/setup-browser-env.js
+++ scripts/setup-browser-env.js
@@ -1,4 +1,4 @@
-require('browser-env')()
+// require('browser-env')()
 
 // "test": "npm run clean && tsc && ava && export BROWSER_TEST=true; ava",
 // if (process.env.BROWSER_TEST) {
--- scripts/setup-test-env.js
+++
@@ -1,7 +0,0 @@
-require('aurelia-polyfills')
-const LogManager = require('aurelia-logging')
-const ColorAppender = require('aurelia-logging-color').ColorAppender
-
-LogManager.addAppender(new ColorAppender())
-
-LogManager.setLevel(LogManager.logLevel.debug)

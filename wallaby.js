bd231f16b1ebfab159aa714dc91b24958e85807f
Homa Wong <homawong@gmail.com>
2019-02-14 02:23:39 -0800

    chore: update project (#69)

--- wallaby.js
+++ wallaby.js
@@ -1,7 +1,11 @@
-module.exports = function (wallaby) {
+module.exports = () => {
   return {
     'files': [
+      { pattern: 'fixtures/**/*', instrument: false },
+      { pattern: 'scripts/*', instrument: false },
+      { pattern: 'package.json', instrument: false },
       { pattern: 'tsconfig.*', instrument: false },
+      { pattern: '__komondor__/**/*', instrument: false },
       'src/**/*.ts',
       '!src/**/*.spec.ts'
     ],
@@ -11,9 +15,31 @@ module.exports = function (wallaby) {
     'env': {
       'type': 'node'
     },
-    compilers: {
-      'src/**/*.ts': wallaby.compilers.typeScript({ module: 'commonjs' }),
+    hints: {
+      allowIgnoringCoverageInTests: true,
+      ignoreCoverage: /istanbul ignore next/
     },
-    testFramework: 'ava'
+    setup(wallaby) {
+      const fs = require('fs');
+      if (fs.patched) return;
+      const path = require('path');
+
+      const writeFile = fs.writeFileSync;
+      fs.writeFileSync = function(file, content) {
+        if (/__komondor__/.test(file)) {
+          writeFile(path.join(wallaby.localProjectDir, file.replace(wallaby.projectCacheDir, '')), content);
+        }
+        return writeFile.apply(this, arguments);
+      }
+      const mkdirSync = fs.mkdirSync;
+      fs.mkdirSync = function (dir, mode) {
+        if (/__komondor__/.test(dir)) {
+          mkdirSync(path.join(wallaby.localProjectDir, dir.replace(wallaby.projectCacheDir, '')), mode);
+        }
+        return mkdirSync.apply(this, arguments);
+      }
+      fs.patched = true;
+    },
+    'testFramework': 'jest'
   }
 }
